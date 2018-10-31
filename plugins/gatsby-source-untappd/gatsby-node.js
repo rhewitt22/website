22f55a538e75536d46fce414b063ec30b66d22f9
Roy Hewitt <roy_hewitt@fws.gov>
2018-10-31 07:50:46 -0400

    Return a promise from plugin

--- plugins/gatsby-source-untappd/gatsby-node.js
+++ plugins/gatsby-source-untappd/gatsby-node.js
@@ -24,25 +24,28 @@ exports.sourceNodes = async (
   const { id, secret } = configOptions
   delete configOptions.plugins
 
-  const checkins = await axios
-    .get(`https://api.untappd.com/v4/user/beers/rhewitt`, {
-      params: {
-        client_id: id,
-        client_secret: secret,
-        limit: 50,
-      },
+  return new Promise(async (resolve, reject) => {
+    const checkins = await axios
+      .get(`https://api.untappd.com/v4/user/beers/rhewitt`, {
+        params: {
+          client_id: id,
+          client_secret: secret,
+          limit: 50,
+        },
+      })
+      .then(res => res.data.response.beers.items)
+      .catch(reject)
+
+    // Todo: Should be able to pull ALL beers, not just most recent 25
+    // Todo: probably don't need to use the .then() to return checkins if I need
+    // to check the 'count' property on the server response. If I have 85 beers
+    // on my profile, and that's greater than the number of beers returned per
+    // server call, I'll have to do another API call with an offset
+
+    checkins.forEach(checkin => {
+      const nodeData = processBeer(checkin, createNodeId, createContentDigest)
+      createNode(nodeData)
     })
-    .then(res => res.data.response.beers.items)
-    .catch(console.log)
-
-  // Todo: Should be able to pull ALL beers, not just most recent 25
-  // Todo: probably don't need to use the .then() to return checkins if I need
-  // to check the 'count' property on the server response. If I have 85 beers
-  // on my profile, and that's greater than the number of beers returned per
-  // server call, I'll have to do another API call with an offset
-
-  checkins.forEach(checkin => {
-    const nodeData = processBeer(checkin, createNodeId, createContentDigest)
-    createNode(nodeData)
+    resolve()
   })
 }
