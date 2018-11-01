97ab6bc36d0cb62bb0732af8553f5f09e558c178
Roy Hewitt <roy_hewitt@fws.gov>
2018-11-01 13:56:39 -0400

    Use emoji component, fix a11y warning

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Emoji from '../components/Emoji'
 
 const About = () => (
   <Layout>
     <h1>About</h1>
-    <p>Hi!</p>
+    <p>
+      Hi <Emoji symbol="👋" label="Waving hand" />
+    </p>
     <p>My name is Roy Hewitt, I'm a web developer living in Atlanta, GA.</p>
   </Layout>
 )
