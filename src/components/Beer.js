fd8de9266a9e046e7c5df6b35cd9b0f09c9e9ef5
Roy Hewitt <roy_hewitt@fws.gov>
2018-11-01 13:55:46 -0400

    Show a stack of cards for beers with a count greater than 1

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -8,20 +8,29 @@ import React from 'react'
 // TODO: Add filtering/sorting
 // TODO: Stack repeats?
 
+const getStackedCardClassName = count => {
+  switch (count) {
+    case 1:
+      return ''
+    case 2:
+      return 'card-stacked-2'
+    case 3:
+      return 'card-stacked-3'
+    default:
+      return 'card-stacked-many'
+  }
+}
+
 const Beer = ({ checkin }) => {
   const { beer, brewery } = checkin
   return (
-    <li
-      style={{
-        marginRight: '1rem',
-        padding: '1rem',
-        border: '1px solid black',
-        flex: '1 1 250px',
-        listStyle: 'none',
-      }}
-    >
-      {beer.beer_name} by {brewery.brewery_name}, {beer.beer_abv}% (
-      {beer.beer_ibu} IBUs)
+    <li className={`card ${getStackedCardClassName(checkin.count)}`}>
+      <h2>
+        {beer.beer_name} by {brewery.brewery_name}
+      </h2>
+      Count: {checkin.count}
+      <br />
+      {beer.beer_abv}% ({beer.beer_ibu} IBUs)
       <br />
       <img src={checkin.beer.beer_label} alt={checkin.beer.beer_name} />
     </li>
